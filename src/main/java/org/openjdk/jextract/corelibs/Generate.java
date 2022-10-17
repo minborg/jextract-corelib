8e1b804c703f71c24f6af4f8c903cbe27be81fea
Per Minborg <pminborg@gmail.com>
2022-10-17 12:49:22 +0200

    Add exec:java target and more

--- src/main/java/org/openjdk/jextract/corelibs/Generate.java
+++ src/main/java/org/openjdk/jextract/corelibs/Generate.java
@@ -4,5 +4,6 @@ public class Generate {
 
     public static void main(String[] args) {
         System.out.println("Generating classes...");
+        org.openjdk.jextract.JextractTool.main(new String[0]);
     }
 }
