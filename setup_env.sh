d3a7376e5265dd304bd00626668bc020ffc04615
Per Minborg <pminborg@gmail.com>
2022-10-18 09:11:45 +0200

    Use module-info.java

---
+++ setup_env.sh
@@ -0,0 +1,2 @@
+export MAVEN_OPTS="--enable-preview --enable-native-access=ALL-UNNAMED -Djextract.debug=true -Djava.library.path=/Users/pemi/git/oracle/minborg-jextract/build/jextract_jmod_libs"
+
--- src/main/java/org/openjdk/jextract/corelibs/Generate.java
+++ src/main/java/org/openjdk/jextract/corelibs/Generate.java
@@ -1,9 +1,32 @@
 package org.openjdk.jextract.corelibs;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Generate {
 
     public static void main(String[] args) {
         System.out.println("Generating classes...");
-        org.openjdk.jextract.JextractTool.main(new String[0]);
+
+        String[] arguments = """
+--source -t netinet
+-I /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/netinet/
+/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/netinet/in.h
+--include-struct sockaddr
+--include-struct sockaddr_in
+--include-struct sockaddr_in6
+--include-macro AF_INET
+--include-macro AF_INET6
+--include-macro AF_UNSPEC""".split("\\s");
+/*                lines().toArray(String[]::new);*/
+
+        var command = "jextract "+ Arrays.stream(arguments).collect(Collectors.joining(" "));
+        System.out.println(command);
+
+        String[] help = new String[]{"--help"};
+        String[] version = new String[]{"--version"};
+
+        org.openjdk.jextract.JextractTool.main(version);
+
     }
 }
